6685d1146755898e7b609f5780bd8ca6f8793ba5
eunsukko <47235893+eunsukko@users.noreply.github.com>
2023-03-08 23:58:28 +0900

    feature: 간단한 config client 를 구현하자

---
+++ src/main/java/com/github/eunsukko/ConfigClient.java
@@ -0,0 +1,30 @@
+package com.github.eunsukko;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequiredArgsConstructor
+public class ConfigClient {
+    @Value("${user.role}")
+    private String role = "role";
+
+    private final Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+
+    @GetMapping(value = "/whoami/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+    }
+}
