52f24020d69047fcd115dd090986af3a7589c93b
eunsukko <47235893+eunsukko@users.noreply.github.com>
2023-09-12 09:51:47 +0900

    feature: 배달/빌링 대사 문제 제출 - 답안 코드를 구현하자

--- src/main/java/com/github/eunsukko/codingtest/Solution2.java
+++ src/main/java/com/github/eunsukko/codingtest/Solution2.java
@@ -1,26 +1,149 @@
 package com.github.eunsukko.codingtest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Solution2 {
-
-    private static final int MAX_NUMBER_OF_DELIVERIES = 100;
 
     public String[][] solution(String[] inputDeliveries, String[] inputBillings, String[] inputQueries) {
         var deliveries = readDeliveries(inputDeliveries);
         var billings = readBillings(inputBillings);
 
-        //
+        var deliveriesForBilling = deliveries.stream()
+                .filter(delivery -> delivery.isBillingRequired())
+                .collect(Collectors.toList());
 
-        return toAnswers(queryForAnswer(inputQueries));
+
+        return toAnswers(queryForAnswer(inputQueries, deliveriesForBilling, billings));
     }
+
+    // 매개변수 등을 여기에 추가해도 좋습니다
+    private List<List<String>> queryForAnswer(String[] inputQueries, List<Delivery> deliveries, List<Billing> billings) {
+        var answers = new ArrayList<List<String>>();
+        for (var comparedDay : inputQueries) {
+            var answer = compareAt(comparedDay, deliveries, billings).stream()
+                    .map(differentDelivery -> differentDelivery.toAnswerString())
+                    .collect(Collectors.toList());
+            answers.add(answer);
+        }
+        return answers;
+    }
+
+    private List<DifferentDelivery> compareAt(String comparedDay, List<Delivery> deliveries, List<Billing> billings) {
+        var deliveriesAt = deliveries.stream()
+                .filter(delivery -> delivery.isAt(comparedDay))
+                .collect(Collectors.toList());
+        var billingsAt = billings.stream()
+                .filter(billing -> billing.isAt(comparedDay))
+                .collect(Collectors.toList());
+        return compare(deliveriesAt, billingsAt);
+    }
+
+    // comparedDay 에 해당하는 배달,빌링 건들만 입력으로 존재합니다
+    private List<DifferentDelivery> compare(List<Delivery> deliveries, List<Billing> billings) {
+        // 복합키 = workingDay + deliveryId
+        var deliveryMap = deliveries.stream()
+                .collect(Collectors.toMap(delivery -> delivery.toKey(), Function.identity()));
+        var billingMap = billings.stream()
+                .collect(Collectors.toMap(billing -> billing.toKey(), Function.identity()));
+
+        var differentDeliveries = new ArrayList<DifferentDelivery>();
+
+        // 배달팀에만 존재하는 경우
+        for (var key : deliveryMap.keySet()) {
+            if (!billingMap.containsKey(key)) {
+                var delivery = deliveryMap.get(key);
+                differentDeliveries.add(DifferentDelivery.deliveryOnly(delivery.getWorkingDay(), delivery.getDeliveryId()));
+            }
+        }
+
+        // 빌링팀에만 존재하는 경우
+        for (var key : billingMap.keySet()) {
+            if (!deliveryMap.containsKey(key)) {
+                var billing = billingMap.get(key);
+                differentDeliveries.add(DifferentDelivery.billingOnly(billing.getWorkingDay(), billing.getDeliveryId()));
+            }
+        }
+
+        // 모두 존재하지만 서로 다른 값을 가진 경우
+        var bothMap = new HashSet<>(deliveryMap.keySet());
+        bothMap.retainAll(billingMap.keySet());
+        for (var key : bothMap) {
+            var delivery = deliveryMap.get(key);
+            var billing = billingMap.get(key);
+
+            DifferentDelivery.compared(delivery, billing)
+                    .ifPresent(differentDelivery -> differentDeliveries.add(differentDelivery));
+        }
+
+        // 문자열 순으로 정렬하기
+        differentDeliveries.sort(Comparator.comparing(differentDelivery -> differentDelivery.toAnswerString()));
+
+        return differentDeliveries;
+    }
+
+    public static class DifferentDelivery {
+        private final String workingDay;
+        private final String deliveryId;
+        private final String differentType; // DELIVERY_ONLY, BILLING_ONLY, DIFFERENT
+
+        public DifferentDelivery(String workingDay, String deliveryId, String differentType) {
+            this.workingDay = workingDay;
+            this.deliveryId = deliveryId;
+            this.differentType = differentType;
+        }
+
+        public static DifferentDelivery deliveryOnly(String workingDay, String deliveryId) {
+            return new DifferentDelivery(workingDay, deliveryId, "DELIVERY_ONLY");
+        }
+
+        public static DifferentDelivery billingOnly(String workingDay, String deliveryId) {
+            return new DifferentDelivery(workingDay, deliveryId, "BILLING_ONLY");
+        }
+
+        public static Optional<DifferentDelivery> compared(Delivery delivery, Billing billing) {
+            var workingDay = delivery.getWorkingDay();
+            var deliveryId = delivery.getDeliveryId();
+
+            if (!delivery.getRiderId().equals(billing.getRiderId())) {
+                return Optional.of(new DifferentDelivery(workingDay, deliveryId, "DIFFERENT"));
+            }
+
+            if (!delivery.getStatus().equals(billing.getDeliveryStatus())) {
+                return Optional.of(new DifferentDelivery(workingDay, deliveryId, "DIFFERENT"));
+            }
+
+            if (delivery.getDeliveryFee() != billing.getDeliveryFee()) {
+                return Optional.of(new DifferentDelivery(workingDay, deliveryId, "DIFFERENT"));
+            }
+
+            return Optional.empty();
+        }
+
+        public String getWorkingDay() {
+            return workingDay;
+        }
+
+        public String getDeliveryId() {
+            return deliveryId;
+        }
+
+        public String getDifferentType() {
+            return differentType;
+        }
+
+        public String toAnswerString() {
+            return String.format("%s %s %s", differentType, workingDay, deliveryId);
+        }
+    }
+
 
     public static class Delivery {
         private final String workingDay;
         private final String riderId;
         private final String deliveryId;
-        private final String status; // CRATED, COMPLETED, CANCELLED
+        private final String status; // COMPLETED, CANCELLED
         private final String requiredBilling; // YES, NO
         private final int deliveryFee;
 
@@ -31,6 +154,38 @@ public class Solution2 {
             this.status = status;
             this.requiredBilling = requiredBilling;
             this.deliveryFee = deliveryFee;
+        }
+
+        public boolean isBillingRequired() {
+            return "YES".equals(requiredBilling);
+        }
+
+        public boolean isAt(String workingDay) {
+            return this.workingDay.equals(workingDay);
+        }
+
+        public String toKey() {
+            return workingDay + deliveryId;
+        }
+
+        public String getWorkingDay() {
+            return workingDay;
+        }
+
+        public String getDeliveryId() {
+            return deliveryId;
+        }
+
+        public String getRiderId() {
+            return riderId;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public int getDeliveryFee() {
+            return deliveryFee;
         }
     }
 
@@ -47,6 +202,34 @@ public class Solution2 {
             this.deliveryId = deliveryId;
             this.deliveryStatus = deliveryStatus;
             this.fee = fee;
+        }
+
+        public boolean isAt(String workingDay) {
+            return this.workingDay.equals(workingDay);
+        }
+
+        public String toKey() {
+            return workingDay + deliveryId;
+        }
+
+        public String getWorkingDay() {
+            return workingDay;
+        }
+
+        public String getDeliveryId() {
+            return deliveryId;
+        }
+
+        public String getRiderId() {
+            return riderId;
+        }
+
+        public String getDeliveryStatus() {
+            return deliveryStatus;
+        }
+
+        public int getDeliveryFee() {
+            return fee;
         }
     }
 
@@ -66,15 +249,6 @@ public class Solution2 {
             bilings.add(new Billing(splited[0], splited[1], splited[2], splited[3], Integer.parseInt(splited[4])));
         }
         return bilings;
-    }
-
-    private List<List<String>> queryForAnswer(String[] inputQueries) {
-        var answer = new ArrayList<List<String>>();
-        for (var workingDay : inputQueries) {
-            //
-            // answer.add();
-        }
-        return answer;
     }
 
     private String[][] toAnswers(List<List<String>> answers) {
--- src/test/java/com/github/eunsukko/codingtest/Solution2Test.java
+++ src/test/java/com/github/eunsukko/codingtest/Solution2Test.java
@@ -8,16 +8,16 @@ class Solution2Test {
     private final Solution2 solution2 = new Solution2();
 
     // 입력
-    // ["20230905 123123 abc CREATED YES 2000"]
-    // ["20230905 123123 abc CREATED 200"]
+    // ["20230905 123123 abc COMPLETED YES 2000"]
+    // ["20230905 123123 abc COMPLETED 200"]
     // ["20230905"]
 
     // 기대하는 결과
     // [["DIFFERENT 20230905 abc"]]
     @Test
     public void testCase1() {
-        var inputDeliveries = new String[]{"20230905 123123 abc CREATED YES 2000"};
-        var inputBillings = new String[]{"20230905 123123 abc CREATED 200"};
+        var inputDeliveries = new String[]{"20230905 123123 abc COMPLETED YES 2000"};
+        var inputBillings = new String[]{"20230905 123123 abc COMPLETED 200"};
         var queries = new String[]{"20230905"};
 
         assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
@@ -25,16 +25,16 @@ class Solution2Test {
     }
 
     // 입력
-    // ["20230905 123123 abc CREATED YES 2000"]
-    // ["20230905 123123 abc CREATED 2000"]
+    // ["20230905 123123 abc COMPLETED YES 2000"]
+    // ["20230905 123123 abc COMPLETED 2000"]
     // ["20230905"]
 
     // 기대하는 결과
     // [[]]
     @Test
     public void testCase2__모두동일한_경우() {
-        var inputDeliveries = new String[]{"20230905 123123 abc CREATED YES 2000"};
-        var inputBillings = new String[]{"20230905 123123 abc CREATED 2000"};
+        var inputDeliveries = new String[]{"20230905 123123 abc COMPLETED YES 2000"};
+        var inputBillings = new String[]{"20230905 123123 abc COMPLETED 2000"};
         var queries = new String[]{"20230905"};
 
         assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
@@ -42,55 +42,72 @@ class Solution2Test {
     }
 
     // 입력
-    // ["20230905 123123 aaa CREATED YES 2000", "20230905 123123 bbb CREATED YES 2000"]
-    // ["20230905 123123 aaa CREATED 2000"]
+    // ["20230905 123123 abc COMPLETED YES 2000", "20230905 123123 bbb COMPLETED YES 2000"]
+    // ["20230905 123123 abc COMPLETED 2000"]
     // ["20230905"]
 
     // 기대하는 결과
     // [["DELIVERY_ONLY 20230905 bbb]]
     @Test
     public void testCase3__배달팀에만_존재하는_경우() {
-        var inputDeliveries = new String[]{"20230905 123123 CREATED YES 2000"};
-        var inputBillings = new String[]{"20230905 123123 CREATED 2000"};
+        var inputDeliveries = new String[]{"20230905 123123 abc COMPLETED YES 2000", "20230905 123123 bbb COMPLETED YES 2000"};
+        var inputBillings = new String[]{"20230905 123123 abc COMPLETED 2000"};
         var queries = new String[]{"20230905"};
 
         assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
-                .isEqualTo(new String[][]{{}});
+                .isEqualTo(new String[][]{{"DELIVERY_ONLY 20230905 bbb"}});
     }
 
     // 입력
-    // ["20230905 123123 aaa CREATED NO 2000"]
-    // ["20230905 123123 aaa CREATED 2000"]
+    // ["20230905 123123 aaa COMPLETED NO 2000"]
+    // ["20230905 123123 aaa COMPLETED 2000"]
     // ["20230905"]
 
     // 기대하는 결과
-    // [["BILLING_ONLY 20230905 aaa]]
+    // [["BILLING_ONLY 20230905 aaa"]]
     @Test
     public void testCase4__빌링팀에만_존재하는_경우() {
-        var inputDeliveries = new String[]{"20230905 123123 CREATED YES 2000"};
-        var inputBillings = new String[]{"20230905 123123 CREATED 2000"};
+        var inputDeliveries = new String[]{"20230905 123123 aaa COMPLETED NO 2000"};
+        var inputBillings = new String[]{"20230905 123123 aaa COMPLETED 2000"};
         var queries = new String[]{"20230905"};
 
         assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
-                .isEqualTo(new String[][]{{}});
+                .isEqualTo(new String[][]{{"BILLING_ONLY 20230905 aaa"}});
     }
 
     // 입력
-    // ["20230905 123123 aaa CREATED NO 2000"]
-    // ["20230905 123123 aaa CREATED 2000"]
+    // ["20230905 123123 aaa COMPLETED NO 2000"]
+    // ["20230905 123123 aaa COMPLETED 2000"]
     // ["20230905", "20230905"]
 
     // 기대하는 결과
     // [["BILLING_ONLY 20230905 aaa], ["BILLING_ONLY 20230905 aaa]]
     @Test
     public void testCase5__동일날짜로_쿼리가_가능합니다() {
-        var inputDeliveries = new String[]{"20230905 123123 CREATED YES 2000"};
-        var inputBillings = new String[]{"20230905 123123 CREATED 2000"};
-        var queries = new String[]{"20230905"};
+        var inputDeliveries = new String[]{"20230905 123123 aaa COMPLETED NO 2000"};
+        var inputBillings = new String[]{"20230905 123123 aaa COMPLETED 2000"};
+        var queries = new String[]{"20230905", "20230905"};
 
         assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
-                .isEqualTo(new String[][]{{}});
+                .isEqualTo(new String[][]{{"BILLING_ONLY 20230905 aaa"}, {"BILLING_ONLY 20230905 aaa"}});
     }
 
-    //
+    // 입력
+    // ["20230905 222222 aaa COMPLETED YES 2000", "20230906 111111 aaa COMPLETED YES 2000", "20230907 111111 aaa COMPLETED YES 2000", "20230905 111111 bbb COMPLETED YES 2000"]
+    // ["20230905 111111 aaa COMPLETED 2000", "20230906 111111 aaa COMPLETED 2000", "20230907 111111 aaa CANCELLED 2000"]
+    // ["20230907", "20230906", "20230905"]
+
+    // 기대하는 결과
+    // [["DIFFERENT 20230907 aaa"], [], ["DELIVERY_ONLY 20230905 bbb", "DIFFERENT 20230905 aaa"]]
+    @Test
+    public void testCase6__여러일자의_데이터가_섞여있습니다() {
+        var inputDeliveries = new String[]{"20230905 222222 aaa COMPLETED YES 2000", "20230906 111111 aaa COMPLETED YES 2000", "20230907 111111 aaa COMPLETED YES 2000", "20230905 333333 bbb COMPLETED YES 2000"};
+        var inputBillings = new String[]{"20230905 111111 aaa COMPLETED 2000", "20230906 111111 aaa COMPLETED 2000", "20230907 111111 aaa CANCELLED 2000", "20230905 444444 ccc CANCELLED 50000"};
+        var queries = new String[]{"20230907", "20230906", "20230905"};
+
+        assertThat(solution2.solution(inputDeliveries, inputBillings, queries))
+                .isEqualTo(new String[][]{{"DIFFERENT 20230907 aaa"}, {}, {"BILLING_ONLY 20230905 ccc", "DELIVERY_ONLY 20230905 bbb", "DIFFERENT 20230905 aaa"}});
+    }
+
+    // 조금은 양이 많은 데이터?
 }
